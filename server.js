4dc3578a6c129e11856697afd80fa6d841dcd0d3
aleksandershurdho20 <aleksandershurdho@gmail.com>
2024-12-09 20:17:49 +0100

    feat: set up frontend

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express();
+
+const PORT = 5000;
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+app.listen(PORT,() => {
+    console.log("running on ",PORT)
+})
+
