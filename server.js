37d40bdeaba39f5035d058f4794052825d69137b
aleksandershurdho20 <aleksandershurdho@gmail.com>
2024-12-09 21:52:17 +0100

    feat: working in progress in ocr reading

--- server.js
+++ server.js
@@ -2,24 +2,49 @@ const express = require("express");
 const tesseract = require("node-tesseract-ocr")
 const { upload } = require("./config/multer");
 const { ocrHandler } = require("./config/ocr");
-
+const fs = require("fs")
 const app = express();
-
+const path = require("path")
 const PORT = 5000;
 
 app.use(express.json());
 
 app.use(express.static("public"));
 
-app.post('/upload', upload.array('files[]', 10), (req, res) => {
-
-    const {files}=req
-
-    console.log(files,"filo")
-
+app.post("/upload", upload.array("files[]", 10), async (req, res) => {
+    const { files } = req;
+  
+  
+  
+    const filePath = path.join(__dirname, "uploads", files[0].filename);
     
-    res.json({ success: true, files: req.files });
-});
+    try {
+      const img = fs.readFileSync(filePath);
+        
+      const text = await tesseract.recognize(img, ocrHandler);
+  
+    //   console.log("OCR Result:", text);
+  
+      res.json({ success: true, files: req.files, text });
+    } catch (error) {
+        console.log(error)
+      res.status(500).json({ success: false, message: "Error during OCR processing.", error: error.message });
+    } finally {
+      fs.unlink(filePath, (err) => {
+        if (err) console.error("Error deleting file:", err.message);
+      });
+    }
+  });
+
+  const test = () => {
+
+    tesseract
+      .recognize("https://tesseract.projectnaptha.com/img/eng_bw.png", { lang: "eng",  oem: 1,
+        psm: 3, })
+      .then((text) => console.log("OCR Result:", text))
+      .catch((error) => console.error("OCR Error:", error.message));
+  }
+  test()
 app.listen(PORT,() => {
     console.log("running on ",PORT)
 })
