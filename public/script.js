4401a165d94daf818c2210b30915141baa64c643
aleksandershurdho20 <aleksandershurdho@gmail.com>
2024-12-09 20:38:39 +0100

    feat: designed frontend

--- public/script.js
+++ public/script.js
@@ -1,11 +1,23 @@
 document.getElementById("file-upload").addEventListener("change", function(event) {
     const {files} = event.target
-    const fileListContainer = document.getElementById("file-list");
+    const uploadButton = document.getElementById('upload-btn');
+    const removeButton = document.getElementById('remove-btn');
+    const fileInput = document.getElementById('file-upload');
+    const statusText = document.getElementById('status');
+    const fileListContainer = document.getElementById('file-list');
     fileListContainer.innerHTML = '';
 
     for (let file of files) {
         const fileItem = document.createElement("p");
         fileItem.textContent = file.name;
         fileListContainer.appendChild(fileItem);
+   
+        console.log(file,"file")
     }
+
+    removeButton.addEventListener('click', function() {
+        fileInput.value = '';  
+        fileListContainer.innerHTML = '';  
+        statusText.textContent = '';  
+    });
 });
